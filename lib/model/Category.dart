5a3409ea78e99d3fc65e36e59e4e86ee8682d415
Bartholome <bartholome.lebruno@gmail.com>
2020-11-19 18:31:49 +0100

    remove unused file

--- lib/model/Category.dart
+++
@@ -1,4 +0,0 @@
-class Category {
-  String name;
-  String type;
-}
--- lib/model/CategoryType.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-class CategoryType {
-  int id;
-  String type;
-
-  CategoryType({@required this.id, this.type});
-}
